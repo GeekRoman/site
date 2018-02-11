d4eea90d2c1e138b74cd19d8edc44eb817823a5b
Roman Sorka <roman311297@gmail.com>
2018-02-11 15:08:39 +0300

    Firts commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+menu.onclick = fuction myFunction(){
+	var x=document.getElemtById("myTopnav")
+
+	if(x.className==="topnav"){
+		x.className +="responsive";
+	} else{
+		x.className="topnav";	
+	}
+}
